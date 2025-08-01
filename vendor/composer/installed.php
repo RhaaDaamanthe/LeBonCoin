30959b60f9cacb36f232b45427d108e96f926478
Jérémy <nicolletjeremy@gmail.com>
2025-08-01 12:13:37 +0200

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'stagiaire/le-bon-coin',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '203db73fb1ba4611cb4dd428f76ac922c02b599f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'altorouter/altorouter' => array(
+            'pretty_version' => '2.0.3',
+            'version' => '2.0.3.0',
+            'reference' => '9931b976423f7334c94f7b5b348be8ab1da3415d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../altorouter/altorouter',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stagiaire/le-bon-coin' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '203db73fb1ba4611cb4dd428f76ac922c02b599f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
